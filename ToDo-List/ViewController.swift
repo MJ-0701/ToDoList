e826f7ea38527c6496a402bfd483dfe22fb2169b
MJ-0701 <jack2718@naver.com>
2022-05-18 18:48:01 +0900

    todo-list end

--- ToDo-List/ViewController.swift
+++ ToDo-List/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var editButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
+    
+    var doneButton : UIBarButtonItem?
+    
     var tasks = [Task](){
         didSet{
             self.saveTasks()
@@ -18,13 +23,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTap))
         self.tableView.dataSource = self
         self.tableView.delegate = self
         self.loadTasks()
         
     }
+    
+    // 셀렉터 타입으로 전달할 메서드를 작성 할 때는 func 키워드 앞에 반드시 @objc를 붙여줘야함
+    @objc func doneButtonTap(){
+        self.navigationItem.leftBarButtonItem = self.editButton
+        self.tableView.setEditing(false, animated: true)
+    }
 
     @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        guard !self.tasks.isEmpty else {return}
+        self.navigationItem.leftBarButtonItem = self.doneButton
+        self.tableView.setEditing(true, animated: true)
     }
     
     @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
@@ -92,7 +107,23 @@ extension ViewController : UITableViewDataSource{
         }
         
         return cell
-        
+    
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        if(self.tasks.isEmpty){
+            self.doneButtonTap()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var tasks = self.tasks
+        let task = tasks[sourceIndexPath.row]
+        tasks.remove(at: sourceIndexPath.row)
+        tasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = tasks
     }
     
     
